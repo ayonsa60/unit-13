84a951da8a388413dddc051698f270e986947c1b
ayonsa60 <ayonsa60@gmail.com>
2021-12-07 20:03:48 -0600

    added framework

---
+++ Develop/routes/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const categoryRoutes = require('./category-routes');
+const productRoutes = require('./product-routes');
+const tagRoutes = require('./tag-routes');
+
+router.use('/api', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
+
+// router.get('/api', categoryRoutes);
+
+module.exports = router;
